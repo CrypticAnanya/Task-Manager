1eaa7c5b50f1030c241a8035260c43a8a8c05300
CrypticAnanya <161688295+CrypticAnanya@users.noreply.github.com>
2026-05-15 16:42:33 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,10 @@
 import { env } from "./config/env.js";
 import { app } from "./app.js";
 
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully 🚀");
+});
+
 app.listen(env.PORT, () => {
   console.log(`Project management API listening on ${env.PORT}`);
 });
